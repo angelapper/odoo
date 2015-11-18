84b9cfa4fc1b7a35d00074b848d670a91833385f
angelapper <shuozhaous@gmail.com>
2015-11-17 17:27:22 -0800

    change to personal project name

--- addons/ce_project/__openerp__.py
+++ addons/ce_project/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Define Angelapper Project Management Permissions
-#    Copyright (C) 2014 ChemEOR
+#    Copyright (C) 2014
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
